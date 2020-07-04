723294d2e9412876c12e9e3e9ba1f48de31774ed
ritwik4690 <vaidyaritwik@gmail.com>
2020-07-04 21:24:34 +0530

    Day 3 progress

---
+++ ecom/ecom/store/urlsstore.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+        #Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+	path('signup/', views.signup, name="signup"),
+	path('index/', views.index, name="index"),
+	path('updateItem/', views.updateItem, name="updateItem"),
+	path('processOrder/', views.processOrder, name="processOrder"),
+	path('men/', views.men, name="men"),
+	path('women/', views.women, name="women"),
+	path('covid/', views.covid, name="covid"),
+	path('other/', views.other, name="other"),
+
+]
